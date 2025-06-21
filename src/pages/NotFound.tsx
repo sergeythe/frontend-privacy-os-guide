3e1ffba21fc94114bc1da0ec170ced947d462838
sergey <>
2025-06-21 22:59:14 +0300

    Add not found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center gap-2 w-full min-h-[50vh] select-none text-sky-700">
+      <h1 className="text-7xl font-extrabold">404</h1>
+      <span className=" text-2xl font-bold">
+        Page <br />
+        not found
+      </span>
+    </div>
+  );
+};
+
+export default NotFound;
