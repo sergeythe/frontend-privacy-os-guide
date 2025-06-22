aea5b6823160fbd25bc3829911d22cf97f7a2b96
sergey <>
2025-06-22 14:11:57 +0300

    Add home page to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import ArticlePage from "@/pages/ArticlePage";
 import NotFound from "@/pages/NotFound";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import HomePage from "./pages/HomePage";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ function App() {
           <main className="container mx-auto bg-sky-200 border-b-6 border-sky-300 my-6 p-6 shadow">
             <Routes>
               <Route path="*" element={<NotFound />} />
+              <Route index path="/" element={<HomePage />} />
               <Route path="/articles/:slug" element={<ArticlePage />} />
             </Routes>
           </main>
